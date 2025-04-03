958b7848a10ecf6a25a01d8afa29b18ecc7dd6d0
ishali <ishali8055@gmail.com>
2025-04-03 19:18:41 +0530

    sidebar update

---
+++ src/Components/main/Main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { assets } from '../../assets/assets'
+
+const Main = () => {
+  return (
+    <div></div>
+  )
+}
+
+export default Main;
---
+++ src/assets/assets.js
@@ -0,0 +1,16 @@
+import add_icon from './add.png';
+import help_icon from './help.png';
+import history_icon from './history.png';
+import menu_icon from './menu.svg';
+import setting_icon from './settings.png';
+import message_icon from './message.png'
+
+
+export const assets ={
+    add_icon,
+    help_icon,
+    history_icon,
+    menu_icon,
+    setting_icon,
+    message_icon
+}
